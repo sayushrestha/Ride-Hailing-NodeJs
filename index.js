9e6efc939cb90a4f327ce95eb7aaa9dd64935cf2
Hilal38 <hhilalkocak@gmail.com>
2024-02-07 20:14:11 +0300

    get passenger detail with id

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.get('/passengers', async(req, res)=> {
     res.render('passengers', {passengers}) // res.render('passengers', {passengers:passengers})
 })
 
+//http://localhost:3000/passenger/d009af8b-b539-466e-ac71-f17037af92a6
+app.get('/passenger/:id', async(req, res)=> {
+    const passenger = await passengerDatabase.find(`${req.params.id}`)
+    if (!passenger) return res.status(404).send('Can not find passenger')
+    res.render('passenger', {passenger})
+})
+
 app.get('/', (req, res) => {
     res.render('index')
 })
