5b058f5b6ba56c3f2d6f94549f81f6f5cde35fb5
Hilal38 <hhilalkocak@gmail.com>
2024-02-07 16:59:14 +0300

    show data in api from db

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const {passengerDatabase} = require('./database')
+const flatted = require('flatted')
 const app = express()
 
+app.get('/passengers', async(req, res)=> {
+    const passengers = await passengerDatabase.load()
+    res.send(flatted.stringify(passengers))
+})
+
+app.get('/', (req, res) => {
+    res.send('homepage')
+})
 app.listen(3000, ()=> {
-    console.log('started listening')
+    console.log('started listening on 3000')
 })
