220242f7c6f208a2fd27ecce30b2eac4fdc7edf1
Hilal38 <hhilalkocak@gmail.com>
2024-02-08 15:39:54 +0300

    book passenger via api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {passengerDatabase} = require('./database')
+const {passengerDatabase, driverDatabase} = require('./database')
 const flatted = require('flatted')
 const Passenger = require('./models/passenger')
 const bodyParser = require('body-parser')
@@ -20,16 +20,26 @@ app.post('/passengers', async(req, res)=> {
     res.send(passenger)
 })
 
-app.delete('/passengers/:id', async(req, res) => {
-    await passengerDatabase.removeBy('id', req.params.id)
+app.delete('/passengers/:passengerId', async(req, res) => {
+    await passengerDatabase.removeBy('passengerId', req.params.passengerId)
     res.send('OK')
 })
 
 //http://localhost:3000/passenger/d009af8b-b539-466e-ac71-f17037af92a6
-app.get('/passenger/:id', async(req, res)=> {
-    const passenger = await passengerDatabase.find(`${req.params.id}`)
+app.get('/passenger/:passengerId', async(req, res)=> {
+    const passenger = await passengerDatabase.find(`${req.params.passengerId}`)
     if (!passenger) return res.status(404).send('Can not find passenger')
     res.render('passenger', {passenger})
+})
+
+//axios.post('/passengers/1021e7e2-9c5b-4fe4-abfe-4a75c76fa4dc/bookings?driverId=65a994cd-031a-4878-aae0-65e6f538934f').then(res=>console.log(res.data)).catch(err=> console.log(err))
+app.post('/passengers/:passengerId/bookings', async(req, res)=> {
+    const passenger = await passengerDatabase.find(req.params.passengerId)
+    const driver = await driverDatabase.find(req.query.driverId)
+    passenger.book(driver, 'SXF', 'Kreuzberg')
+    await passengerDatabase.update(passenger)
+    res.send(flatted.stringify(passenger))
+
 })
 
 app.get('/', (req, res) => {
