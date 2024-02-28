8d6c125f6b98bb9eb75a9f0c1a3a0750c53587ac
Hilal38 <hhilalkocak@gmail.com>
2024-02-28 18:50:18 +0300

    Add local state increment decrement sample

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -13,7 +13,7 @@ export default {
 <template lang="pug">
   .about
     h1 This is a journey about seeking yourself
-    HelloWorld(msg="Computer Science")
+    HelloWorld( msg="Computer Science")
  
 </template>
 
